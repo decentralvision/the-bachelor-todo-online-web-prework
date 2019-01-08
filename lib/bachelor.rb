d8f86d393285dc184ef8f872a4dcecb5a05252ab
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-08 19:23:18 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,38 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+  name_array = []
+  data.each do |season_hsh, data|
+    if season_hsh == season
+      data.each do |key, value|
+        if key.fetch("status") == "Winner"
+          name_array = key.fetch("name").split(" ")
+        end
+      end
+    end
+  end
+  name_array[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name = ''
+  data.each do |season_hsh, data_array|
+      data_array.each do |data|
+        data.each do |key, value|
+          if value == occupation
+            name = data['name']
+          end
+        end
+      end
+  end
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  hometown_count = 0
+  data.each do |season, season_arr|
+    season_arr.each do |key, value|
+    end
+  end
 end
 
 def get_occupation(data, hometown)
